cf48225e4cba9be4a8262c9aeb5a7197e440d7aa
MilttO0nMss <grountish@gmail.com>
2020-07-04 17:41:29 +0200

    finished

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 let slider
 let bugs = []; // array of Jitter objects
+let bugs2 = []; // array of Jitter objects
 let parr
-
+let img
+function preload() {
+    img = loadImage('./resources/hand.png')
+}
 function setup() {
     let cnv = createCanvas(800, 400);
     cnv.parent('#data-canvas-container')
 
-    slider = createSlider(0, 120, 0, 1)
-    for (let i = 0; i < 120; i++) {
+    slider = createSlider(0, 216, 0, 1)
+    for (let i = 0; i < 216; i++) {
         bugs.push(new Jitter());
+    }
+    for (let i = 0; i < 90; i++) {
+        bugs2.push(new Jitter2());
     }
     slider.style('width', '800px')
     slider.parent('#slider-container')
@@ -23,7 +30,11 @@ function draw() {
         bugs[i].move();
         bugs[i].display();
     }
-    
+    for (let i = 0; i < map(slider.value(), 0, 216, 0, 90); i++) {
+        bugs2[i].move();
+        bugs2[i].display();
+    }
+   
 }
 
 // Jitter class
@@ -41,18 +52,37 @@ class Jitter {
     }
 
     display() {
+        fill(255)
         ellipse(this.x, this.y, this.diameter, this.diameter);
     }
 }
+class Jitter2 {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.diameter = random(-5, 30);
+        this.speed = random(2);
+    }
 
-let numOf = document.getElementById('number-of-x')
+    move() {
+        this.x += random(-this.speed, this.speed);
+        this.y += random(-this.speed, this.speed);
+    }
+
+    display() {
+        fill(0)
+        rect(this.x, this.y, this.diameter, this.diameter);
+    }
+}
+
+let numOf = document.getElementById('number-of-x');
+let numOfY = document.getElementById('number-of-y');
 
 setTimeout(() => {
 
     let slider1 = document.getElementsByTagName('input')[0]
     slider1.addEventListener('change', () => {
-        numOf.innerHTML = 'Number of X = ' + slider1.value
+        numOf.innerHTML = '⚪ Boys provided with residential Care = ' + slider1.value;
+        numOfY.innerHTML = '⬛ Boys supported through day-care facibilities = ' +floor(map(slider.value(), 0, 216, 0, 90))  ;
     })
-
-
-}, 100);
+}, 1000);
