209752bd43ca361d2f43447dad37cd09b92428d4
MilttO0nMss <grountish@gmail.com>
2020-07-04 16:30:51 +0200

    s

---
+++ sketch.js
@@ -0,0 +1,64 @@
+let slider
+let bugs = []; // array of Jitter objects
+let parr
+
+function setup() {
+    let cnv = createCanvas(800, 400);
+    cnv.parent('#data-canvas-container')
+
+    slider = createSlider(0, 120, 0, 1)
+    for (let i = 0; i < 120; i++) {
+        bugs.push(new Jitter());
+    }
+    slider.style('width', '800px')
+    slider.parent('#slider-container')
+    slider.class('slider')
+
+}
+
+function draw() {
+    strokeWeight(.1)
+    background(slider.value(), 89, 100, 40);
+    for (let i = 0; i < slider.value(); i++) {
+        bugs[i].move();
+        bugs[i].display();
+    }
+    for (let i = 0; i < slider.value(); i++) {
+        // line(random(800), random(400), random(800), random(400))
+
+        translate(p5.Vector.fromAngle(millis() / 1000, 40));
+        pop()
+        ellipse(i + 1, i + 1, i + 2, i + 2)
+    }
+}
+
+// Jitter class
+class Jitter {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.diameter = random(-5, 50);
+        this.speed = random(1);
+    }
+
+    move() {
+        this.x += random(-this.speed, this.speed);
+        this.y += random(-this.speed, this.speed);
+    }
+
+    display() {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+}
+
+let numOf = document.getElementById('number-of-x')
+
+setTimeout(() => {
+
+    let slider1 = document.getElementsByTagName('input')[0]
+    slider1.addEventListener('change', () => {
+        numOf.innerHTML = 'Number of X = ' + slider1.value
+    })
+
+
+}, 100);
